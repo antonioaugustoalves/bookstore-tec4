d5fa7a0e90b08b13c37098955f4484bf695c03d4
Antonio <antonio@gmailcom>
2022-09-26 13:10:25 -0300

    Criação da rota de listagem de categorias

--- src/routes/categorias.routes.ts
+++ src/routes/categorias.routes.ts
@@ -1,10 +1,13 @@
 import {Router} from "express";
+import { ListarCategoriasController } from "../modules/categorias/useCases/listarCategorias/ListarCategoriasController";
 import { NovaCategoriaController } from "../modules/categorias/useCases/novaCategoria/NovaCategoriaController";
 
 const categoriasRoutes = Router();
 
 const novaCategoriaController = new NovaCategoriaController();
+const listarCategoriasController = new ListarCategoriasController();
 
 categoriasRoutes.post("/", novaCategoriaController.handle);
+categoriasRoutes.get("/", listarCategoriasController.handle);
 
 export { categoriasRoutes}
